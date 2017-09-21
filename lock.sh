53804c7739048cec041b8b70e6b5a70dc2e5eb09
Gleb Storozhik <hello@noobusdeer.com>
2017-09-21 17:59:52 +0300

    update i3lock script

--- lock.sh
+++ lock.sh
@@ -1,3 +1,14 @@
-   #!/bin/bash 
+#!/bin/bash
+#
 
-   i3lock -i /home/noobusdeer/Pictures/lock.png -n -u
+ICON_LOCK=/home/noobusdeer/Pictures/licon.png
+TEMP_FILE=/tmp/lock.png
+
+MOGRIFY_OPTS="-scale 10% -scale 1000% $TEMP_FILE"
+CONVERT_OPTS="$TEMP_FILE  $ICON_LOCK -gravity center -composite $TEMP_FILE "
+
+rm $TEMP_FILE
+scrot $TEMP_FILE
+mogrify $MOGRIFY_OPTS
+convert $CONVERT_OPTS
+i3lock -i $TEMP_FILE -n -u
