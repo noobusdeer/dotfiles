f3e12d0f43fc7591ffe09806ca47f53c6ac60f77
Gleb Storozhik <hello@noobusdeer.com>
2017-10-03 19:00:40 +0300

    some color fix

--- .config/scripts/xrandr2560x1440-nvidia.sh
+++ .config/scripts/xrandr2560x1440-nvidia.sh
@@ -2,3 +2,4 @@
 xrandr --newmode "2560x1440_30.00"  146.25  2560 2680 2944 3328  1440 1443 1448 1468 -hsync +vsync
 xrandr --addmode HDMI-1-2 2560x1440_30.00
 xrandr --output eDP-1-1 --off --output HDMI-1-2 --mode 2560x1440_30.00
+
