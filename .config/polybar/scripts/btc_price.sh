f739dc058db696ecf376744e727662b828b36f6e
Gleb Storozhik <hello@noobusdeer.com>
2018-07-18 19:20:27 +0300

    change polybar for openbox

---
+++ .config/polybar/scripts/btc_price.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rate=$(curl -sf curl https://api.coinmarketcap.com/v2/ticker/1/ | grep "price" | grep -oE "[0-9]*\....." | colrm 5 )
+rate=$(LANG=C printf "%.2f" "$rate")
+
+echo "$rate "
